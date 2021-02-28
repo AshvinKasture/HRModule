8b41953646a5c2fed4ce6c6448c7be592bfccfc1
Shrikant Pandhekar <shrikantp9762@gmail.com>
2021-02-28 14:45:07 +0530

    Staff Module Added
    - Add staff member
    - List of Staff
    - Leave management
    - Salary Management

---
+++ navbar.php
@@ -0,0 +1,16 @@
+
+<div class="sidebar">
+        <h2>HR Module</h2>
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i>Dashboard</a></li>
+            <li><a href="addStaff.php"><i class="fas fa-project-diagram"></i>Add Staff Member</a></li>
+            <li><a href="listStaff.php"><i class="fas fa-blog"></i>List Of Staff</a></li>
+            <li><a href="leave_management.php"><i class="fas fa-user"></i>Apply Leave</a></li>
+            <li><a href="leavehistory.php"><i class="fas fa-address-card"></i>Leave History</a></li>
+            <li><a href="staffSalary.php"><i class="fas fa-project-diagram"></i>Salary</a></li>
+            <li><a href="#"><i class="fas fa-blog"></i>Blogs</a></li>
+            <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
+            <li><a href="logout.php"><i class="fas fa-map-pin"></i>Log Out</a></li>
+        </ul> 
+</div>
+    
